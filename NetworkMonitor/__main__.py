f72a12c461b65dcef162f840c65bc249ea899feb
jaypyles <jpylesbusiness@gmail.com>
2023-05-25 23:16:02 -0500

    feat: updated frontend to use React

--- NetworkMonitor/__main__.py
+++ NetworkMonitor/__main__.py
@@ -1,18 +1,18 @@
 # PDM
-from flask import Flask, render_template
+from flask import Flask
 
 # LOCAL
 from NetworkMonitor.routes.system_api import system_bp
 from NetworkMonitor.routes.network_speed_api import network_speed_bp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../my-react-app/build/", static_url_path="/")
 app.register_blueprint(network_speed_bp, url_prefix="/routes")
 app.register_blueprint(system_bp, url_prefix="/routes")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return app.send_static_file("index.html")
 
 
 if __name__ == "__main__":
